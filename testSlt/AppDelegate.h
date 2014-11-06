af168be903dfbe80ada6c16d653265aa45ea9323
Resory <resory@126.com>
2014-11-06 10:34:12 +0800

    Initial Commit

---
+++ testSlt/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  testSlt
+//
+//  Created by Resory on 14/11/6.
+//  Copyright (c) 2014年 Resory. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ testSlt/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  testSlt
+//
+//  Created by Resory on 14/11/6.
+//  Copyright (c) 2014年 Resory. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
